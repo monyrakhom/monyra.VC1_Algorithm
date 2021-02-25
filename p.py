3db0de78f279d2adedcf60db7d40411a2d3fecd7
monyrakhom <monyra.khom@student.passerellesnumeriques.org>
2021-02-25 22:45:29 +0700

    vc1 first

--- p.py
+++ p.py
@@ -1,26 +1,18 @@
 from tkinter import *
-import random
 # Create an empty window
 windows = Tk()
 # Set TK window size w & h
 windows.geometry("800x800")
 
 windows.title("WELCOMENEW GAME")
-# CONSTANT
-COLORS_OF_CANDIES = ["#18ffff", "#b388ff", "#90caf9", "#00695c"] #color of candy
-
-# GLOBALVARIABLES
-
-
-# WINDOWS
 
 wid =  800
 hei = 600
 posX = int(wid/2)
 posY = int(hei/2)
-my_canvas = Canvas(windows, width = wid, height = hei , bg="#37474f")
+my_canvas = Canvas(windows, width = wid, height = hei , bg="red")
 my_canvas.pack(pady=20)
-my_circle = my_canvas.create_rectangle(posX,posY,posX+50,posY+50 ,fill=random.choice(COLORS_OF_CANDIES))
+my_circle = my_canvas.create_rectangle(posX,posY,posX+50,posY+50 ,fill="white")
 
 
 def moveLeft(event):
@@ -55,3 +47,5 @@ windows.bind("<Up>", moveUp)
 windows.bind("<Down>", moveDown)
 # Display all
 windows.mainloop()
+
+    
