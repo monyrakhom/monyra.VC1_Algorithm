1d7cb7f1bf95f17cd979b102943d038ab879f57e
monyrakhom <monyra.khom@student.passerellesnumeriques.org>
2021-02-25 16:58:43 +0700

    version first

---
+++ VC1_Algorithm.py
@@ -0,0 +1,31 @@
+import tkinter as tk
+import random
+colors = ["red", "blue", "black", "yellow"] #color of candy
+def movesnake():
+    global snake, position
+    for i in range(len(snake)):
+        snake = snake[i]
+        # [x0, y0, x1, y1]
+
+
+
+
+
+
+
+
+
+
+canvas.pack(expand=True, fill='both')
+frame.pack(expand=True, fill='both')
+windows.bind("<Right-click", ....)
+windows.bind("<Left-click", ....)
+windows.bind("<Top-click", ....)
+windows.bind("<Buttom-click", ....)
+
+
+
+# Display all
+
+
+windows.mainloop()
