0deb74b2695eb1b849408dce0c91ba148298a00f
monyrakhom <monyra.khom@student.passerellesnumeriques.org>
2021-02-27 12:05:38 +0700

    VC1 third

--- VC1_Algorithm.py
+++ VC1_Algorithm.py
@@ -1,57 +1,53 @@
-from tkinter import *
-import random
-# Create an empty window
-windows = Tk()
-# Set TK window size w & h
-windows.geometry("800x800")
+import tkinter as tk
 
-windows.title("WELCOMENEW GAME")
-# CONSTANT
-COLORS_OF_CANDIES = ["#18ffff", "#b388ff", "#90caf9", "#00695c"] #color of candy
-
-# GLOBALVARIABLES
-
-
-# WINDOWS
-
-wid =  800
-hei = 600
-posX = int(wid/2)
-posY = int(hei/2)
-my_canvas = Canvas(windows, width = wid, height = hei , bg="#37474f")
-my_canvas.pack(pady=20)
-my_circle = my_canvas.create_rectangle(posX,posY,posX+50,posY+50 ,fill=random.choice(COLORS_OF_CANDIES))
-
-
+root = tk.Tk()
+# Set TK window size to width 600 px and height 200 px
+root.geometry("600x600")
+# Create a frame in the window (frame is a container, like "div" in HTML)
+frame = tk.Frame()
+canvas = tk.Canvas(frame)
+# Set the title of the frame
+frame.master.title("Hello PNC")
+# constants
+oval=canvas.create_rectangle(0,0,50,50,fill="red")
+# movement in x direction
+x=1
+# movement in y direction
+y=0
+def moveSnake():
+    canvas.move(oval,x,y)
+    canvas.after(500,lambda:moveSnake())
 def moveLeft(event):
-    posX = -10
-    posY  = 0
-    my_canvas.move(my_circle,posX,posY)
-
+    global x,y
+    x=-10
+    y=0
+ 
+    
 
 def moveRight(event):
+    global x,y
+    x=10
+    y=0
     
-    posX = 10
-    posY  = 0
-    my_canvas.move(my_circle,posX,posY)
-
+   
 def moveUp(event):
-    posX = 0
-    posY  = -10
-    my_canvas.move(my_circle,posX,posY)
-
-
+    global x,y
+    x=0
+    y=-10
+    
+  
 def moveDown(event):
-    posX = 0
-    posY  = 10
-    my_canvas.move(my_circle,posX,posY)
-
-
-
-
-windows.bind("<Left>", moveLeft)
-windows.bind("<Right>", moveRight)
-windows.bind("<Up>", moveUp)
-windows.bind("<Down>", moveDown)
+    global x,y
+    x=0
+    y=10
+   
+    
+root.bind("<Left>",moveLeft)
+root.bind("<Right>",moveRight)
+root.bind("<Up>",moveUp)
+root.bind("<Down>",moveDown)
+canvas.pack(expand=True, fill='both')
+frame.pack(expand=True, fill='both')
+moveSnake()
 # Display all
-windows.mainloop()
+root.mainloop()
